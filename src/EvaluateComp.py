f155b9bf6e8a506548d5ee2c8ea80505dd5d80fd
wengzehang <zehangweng@gmail.com>
2022-04-04 11:25:46 +0200

    evaluate comp

--- src/EvaluateComp.py
+++ src/EvaluateComp.py
@@ -1,5 +1,6 @@
 import os, sys, torch, importlib, argparse, numpy as np, trimesh
 from tqdm import tqdm
+from util.Torch_Utility import chamfer_distance_with_batch
 import matplotlib.pyplot as plt
 
 sys.path.append('util')
@@ -33,9 +34,10 @@ def parse_args():
     parser.add_argument('--restore_path', type=str, help='path to saved pre-trained model [default: ]')
     parser.add_argument('--steps_eval', type=int, default=1000, help='# steps to evaluate [default: 1e3]')
     parser.add_argument('--epochs_save', type=int, default=5, help='# epochs to save [default: 5 epochs]')
-    parser.add_argument("--tasklist", nargs="+", default=None)
+    parser.add_argument("--tasklist", nargs="+", default=[1, 2])
     parser.add_argument('--numkp', type=int, default=3, help='number of keypoints [default: 3]')
-    parser.add_argument('--ck_path', type=str, help='path to save trained model [default: ]')
+    parser.add_argument('--detector_ck_path', type=str, help='path to the trained detector model [default: ]')
+    parser.add_argument('--decoder_ck_path', type=str, help='path to save decoder model [default: ]')
     parser.add_argument('--data_path', type=str, help='path to the dataset')
 
     ''' === Model Setting === '''
@@ -45,120 +47,84 @@ def parse_args():
     parser.add_argument('--augocc', action='store_true', help='occlusion augmentation [default: False]')
     parser.add_argument('--augsca', action='store_true', help='scaling augmentation [default: False]')
     parser.add_argument('--k', type=int, default=20, help='# nearest neighbors in DGCNN [20]')
-    parser.add_argument('--grid_size', type=int, default=4, help='edge length of the 2D grid [4]')
-    parser.add_argument('--grid_scale', type=float, default=0.05, help='scale of the 2D grid [0.5]')
-    parser.add_argument('--num_coarse', type=int, default=64, help='# points in coarse gt [1024]')
+    parser.add_argument('--grid_size', type=int, default=4, help='edge length of the 2D grid [4]') # 4
+    parser.add_argument('--grid_scale', type=float, default=0.05, help='scale of the 2D grid [0.5]') # 0.5
+    parser.add_argument('--num_coarse', type=int, default=64, help='# points in coarse gt [64]') # 1024 , change to 64
+    parser.add_argument('--num_fine', type=int, default=1024, help='# points in fine [1024]]') # 1024 , change to 64
     parser.add_argument('--emb_dims', type=int, default=1024, help='# dimension of DGCNN encoder [1024]')
     parser.add_argument('--input_pts', type=int, default=1024, help='# points of occluded inputs [1024]')
-    parser.add_argument('--gt_pts', type=int, default=1024, help='# points of ground truth inputs [1024]')
-
-    ''' === Testing Setting === '''
-    parser.add_argument("--seqid", nargs="+", default=[])
+    parser.add_argument('--gt_pts', type=int, default=1024, help='# points of ground truth inputs [1024]') # 16384
 
     return parser.parse_args()
 
 
-def main(args, task_index, num_kp=3):
+def main(args, task_index):
     '''
     keypoint detection main
     '''
 
+    numkp = 3
+
     ''' === Set up Task and Load Data === '''
     root = args.data_path
     print("load data from {}".format(root))
-    kp_dict_file = "src/kp_ind_list.pickle"
-    NUM_KP = 31
+    # kp_dict_file = "src/kp_ind_list.pickle"
 
-    checkpoints_dir = os.path.join(args.ck_path, "{}_{}_{}_{}/{}/{}/".format(args.model, args.augrot, args.augocc, args.augsca, task_index, args.numkp))
+    # checkpoints_dir = os.path.join(args.ck_path, "{}_{}_{}_{}/{}/{}/".format(args.model, args.augrot, args.augocc, args.augsca, task_index, args.numkp))
+    detector_checkpoints_dir = os.path.join(args.detector_ck_path, "{}_{}_{}_{}/{}/{}/".format(args.model, args.augrot, args.augocc, args.augsca, task_index, numkp))
+    completor_checkpoints_dir = os.path.join(args.decoder_ck_path, "{}_{}_{}_{}/{}/{}/".format(args.model, args.augrot, args.augocc, args.augsca, task_index, numkp))
 
-    if os.path.exists(checkpoints_dir) is False:
-        print("invalid checkpoint path")
+
+    if os.path.exists(detector_checkpoints_dir) is False:
+        print("invalid detector checkpoint path")
+
+    if os.path.exists(completor_checkpoints_dir) is False:
+        print("invalid completor/decoder checkpoint path")
 
 
     ''' === Load Model and Backup Scripts === '''
-    MODEL = importlib.import_module(args.model)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # load the detector
+    MODEL = importlib.import_module(args.model)
     detector = MODEL.get_model(args=args, grid_size=args.grid_size,
                                 grid_scale=args.grid_scale, num_coarse=args.num_coarse, num_channel=3, num_cp = NUM_KP).to(device)
 
     detector = torch.nn.DataParallel(detector)
-    print('=' * 27)
-    print('Using %d GPU,' % torch.cuda.device_count(), 'Indices: %s' % args.gpu)
-    print('=' * 27)
+    # load the decoder
+    MODEL_COMP = importlib.import_module("decoder_comp")
+    completor = MODEL_COMP.get_model(args=args, grid_size=args.grid_size,
+                                   grid_scale=args.grid_scale, num_coarse=args.num_coarse, num_fine=args.num_fine, num_channel=3,
+                                   num_cp=numkp).to(device)
+    completor = torch.nn.DataParallel(completor)
 
-    ''' === Restore Model from Checkpoints, If there is any === '''
+    ''' === Restore detector Model from Checkpoints, If there is any === '''
 
-    bestepoch = np.max([int(ckfile.split('.')[0].split('_')[-1]) for ckfile in os.listdir(checkpoints_dir)])
-    args.restore_path_root = os.path.join(checkpoints_dir, "model_epoch_{}.pth".format(bestepoch))
+    bestepoch_detector = np.max([int(ckfile.split('.')[0].split('_')[-1]) for ckfile in os.listdir(detector_checkpoints_dir)])
+    args.restore_path_root = os.path.join(detector_checkpoints_dir, "model_epoch_{}.pth".format(bestepoch_detector))
     detector.load_state_dict(torch.load(args.restore_path_root)['model_state_dict'])
     detector.eval()
+
+    ''' === Restore decoder Model from Checkpoints, If there is any === '''
+
+    bestepoch_decoder = np.max([int(ckfile.split('.')[0].split('_')[-1]) for ckfile in os.listdir(completor_checkpoints_dir)])
+    args.restore_path_root = os.path.join(completor_checkpoints_dir, "model_epoch_{}.pth".format(bestepoch_decoder))
+    detector.load_state_dict(torch.load(args.restore_path_root)['model_state_dict'])
+    completor.eval()
 
     '''
        load the dataset
        '''
 
     root = "../data/"
-    tasks_path = os.path.join(root, "h5data/tasks/pointclouds_sim")
+    tasks_path = os.path.join(root, "h5data/tasks")
     print("Chosen task:", task_index)
-    task = Datasets.get_task_by_index(task_index)
 
-    H5DataPath = os.path.join(tasks_path, "{}_{}_clean.h5".format(task_index, 'train'))
-    TRAIN_DATASET = General_PartKPDataLoader_HDF5(H5DataPath, augrot=args.augrot, augocc=args.augocc, augsca=args.augsca, ref="left")
-    # VAL
-    H5DataPath = os.path.join(tasks_path, "{}_{}_clean.h5".format(task_index, 'valid'))
-    VAL_DATASET = General_PartKPDataLoader_HDF5(H5DataPath, augrot=args.augrot, augocc=args.augocc, augsca=args.augsca, ref="left")
     # TEST
     H5DataPath = os.path.join(tasks_path, "{}_{}_clean.h5".format(task_index, 'test'))
     TEST_DATASET = General_PartKPDataLoader_HDF5(H5DataPath, augrot=args.augrot, augocc=args.augocc, augsca=args.augsca, ref="left")
 
 
-    trainDataLoader = DataLoader(TRAIN_DATASET, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
-    valDataLoader = DataLoader(VAL_DATASET, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=False)
     testDataLoader = DataLoader(TEST_DATASET, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=False)
-
-    # evaluation
-    with tqdm(trainDataLoader, unit="batch") as tepoch:
-        batchcount = 0
-        trn_errorlist = []
-        # for  points, target, _ in tepoch:
-        for points, target, ref, sid, fid in tepoch:
-            batchcount += 1
-            points, target = points.transpose(2, 1).float().cuda(), target.float().cuda()
-
-            if args.model == 'pointnet_det':
-                pred, trans_feat = detector(points)
-            elif args.model == 'pcn_det':
-                pred = detector(points)
-            elif args.model == 'pcn_det_comp':
-                pred, coarse, fine = detector(points)
-            else:
-                raise NotImplementedError("only support pointnet_det, pcn_det, pcn_det_comp")
-
-
-            batcherror = torch.linalg.norm(target - pred, axis=-1).mean(axis=-1).data.cpu().numpy()
-            trn_errorlist = np.hstack((trn_errorlist, batcherror))
-
-
-
-    with tqdm(valDataLoader, unit="batch") as tepoch:
-        batchcount = 0
-        val_errorlist = []
-        # for  points, target, _ in tepoch:
-        for points, target, ref, sid, fid in tepoch:
-            batchcount += 1
-            points, target = points.transpose(2, 1).float().cuda(), target.float().cuda()
-
-            if args.model == 'pointnet_det':
-                pred, trans_feat = detector(points)
-            elif args.model == 'pcn_det':
-                pred = detector(points)
-            elif args.model == 'pcn_det_comp':
-                pred, coarse, fine = detector(points)
-            else:
-                raise NotImplementedError("only support pointnet_det, pcn_det, pcn_det_comp")
-
-            batcherror = torch.linalg.norm(target - pred, axis=-1).mean(axis=-1).data.cpu().numpy()
-            val_errorlist = np.hstack((val_errorlist, batcherror))
 
     with tqdm(testDataLoader, unit="batch") as tepoch:
         batchcount = 0
@@ -167,47 +133,52 @@ def main(args, task_index, num_kp=3):
         for points, target, ref, sid, fid in tepoch:
             batchcount += 1
             points, target = points.transpose(2, 1).float().cuda(), target.float().cuda()
-
-            if args.model == 'pointnet_det':
-                pred, trans_feat = detector(points)
-            elif args.model == 'pcn_det':
-                pred = detector(points)
-            elif args.model == 'pcn_det_comp':
-                pred, coarse, fine = detector(points)
+            if args.model == 'pcn_det':
+                pred_kp = detector(points)
             else:
-                raise NotImplementedError("only support pointnet_det, pcn_det, pcn_det_comp")
+                raise NotImplementedError("only support pcn_det for testing here")
 
-            batcherror = torch.linalg.norm(target - pred, axis=-1).mean(axis=-1).data.cpu().numpy()
+            _, pc_fine = completor(pred_kp)
+            fine_loss_batch_bidirection = chamfer_distance_with_batch(pc_fine.permute(0, 2, 1), points)
+            fine_chamferdist_batch = (fine_loss_batch_bidirection[0] + fine_loss_batch_bidirection[1])
+
+
+            batcherror = fine_chamferdist_batch.data.cpu().numpy()
+            print(batcherror.shape)
             test_errorlist = np.hstack((test_errorlist, batcherror))
 
-    return trn_errorlist, val_errorlist, test_errorlist
+    return test_errorlist
 
 if __name__ == '__main__':
     '''
-        python src/Evaluate_acc_sim.py --batch_size 256 --model pointnet_det --augrot --augocc --augsca  '''
+        python src/Evaluate_acc_sim.py --batch_size 256 --model pcn_det --augrot --augocc --augsca  
+        
+        --data_path /local_storage/users/zehang/keypoint_humanoids/data --detector_ck_path /nas/zehang/keypoint_humanoids/detect_checkpoint --decoder_ck_path /nas/zehang/keypoint_humanoids/kp2comp_checkpoint --batch_size 32 --model pcn_det --augrot --augocc --augsca --savemodel --tasklist 2 4 6 8 10 12 14 16 18 20--numkp 3
+        
+        '''
     args = parse_args()
     task_index_list = args.tasklist
     if task_index_list == None:
         task_index_list = np.arange(1,21)
         print("test all")
 
-    num_kp_list = [3,5,10,15,20,25,30,35,40,45,50,55,60]
+    test_error_list = main(args, task_index=2)
 
-    for num_kp in num_kp_list:
-        trn_error_tasks = []
-        val_error_tasks = []
-        test_error_tasks = []
-        for task_index in task_index_list:
-            trn_error_list, val_error_list, test_error_list = main(args, task_index=task_index, num_kp=num_kp)
-            trn_error_tasks.append(trn_error_list)
-            val_error_tasks.append(val_error_list)
-            test_error_tasks.append(test_error_list)
+    # num_kp_list = [3,5,10,15,20,25,30,35,40,45,50,55,60]
+    #
+    # for num_kp in num_kp_list:
+    #     trn_error_tasks = []
+    #     val_error_tasks = []
+    #     test_error_tasks = []
+    #     for task_index in task_index_list:
+    #         trn_error_list, val_error_list, test_error_list = main(args, task_index=task_index)
+    #         trn_error_tasks.append(trn_error_list)
+    #         val_error_tasks.append(val_error_list)
+    #         test_error_tasks.append(test_error_list)
 
-        error_total = [trn_error_tasks, val_error_tasks, test_error_tasks]
-
-        import pickle
-        errorfile = open("logs/exp1_sim_{}.acc".format(args.model), 'wb')
-        pickle.dump(error_total, errorfile)
-        errorfile.close()
-
-
+        # error_total = [trn_error_tasks, val_error_tasks, test_error_tasks]
+        #
+        # import pickle
+        # errorfile = open("logs/exp1_sim_{}.acc".format(args.model), 'wb')
+        # pickle.dump(error_total, errorfile)
+        # errorfile.close()
